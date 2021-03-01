f0abe13ee306cb74bbca741b7cec1892f914d5f9
daryl <tsdaryl@gmail.com>
2021-03-01 13:10:17 -0800

    add context provider for search results

--- application/api/views.py
+++ application/api/views.py
@@ -8,6 +8,30 @@ api = Blueprint('api', __name__, template_folder = 'templates/api')
 @api.route('/search', methods=['get'])
 # @login_required
 def index():
-  query_term = 'emp'
+  query_term = 'stack'
   results = Pages.query.filter(Pages.instructions.contains(query_term)).all()
+
+  def process_instructions(raw):
+    searchable_raw = raw.lower()
+    target_start_loc = searchable_raw.find(query_term.lower())
+    target_end_loc = target_start_loc + len(query_term)
+    target_before = raw[target_start_loc - 50: target_start_loc]
+    target_after = raw[target_end_loc: target_end_loc + 50]
+
+    return '...' + target_before + f"<span>{query_term}</span>" + target_after + '...'
+
+  processed_results = [
+    {
+      'ref_id': result.ref_id,
+      'heading': result.heading,
+      'type': result.activity_type,
+      'context': process_instructions(result.instructions),
+      'duration': result.duration,
+      'URL': result.link,
+      'date': result.date_recorded
+    } for result in results
+  ]
+
+  for result in processed_results:
+    print(result['context'])
   return render_template('index.html')
