e7cbf0198d7a4bd43292538034db25a9ae422a1c
Daryl <tsdaryl@gmail.com>
2021-03-01 12:20:02 -0800

    add api route for searches

--- application/api/views.py
+++ application/api/views.py
@@ -1,15 +1,13 @@
 from flask import render_template, Blueprint, request, jsonify, make_response
 from application import db
 from flask_login import login_user, login_required, logout_user, current_user
+from application.models import Pages
 
 api = Blueprint('api', __name__, template_folder = 'templates/api')
 
-@api.route('/', methods=['post', 'get'])
+@api.route('/search', methods=['get'])
+# @login_required
 def index():
+  query_term = 'emp'
+  results = Pages.query.filter(Pages.instructions.contains(query_term)).all()
   return render_template('index.html')
-
-
-@api.route('/search_results', methods=['get'])
-@login_required
-def search_results():
-  pass
